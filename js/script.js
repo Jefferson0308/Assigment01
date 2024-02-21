539ab0d44fee523410c4b68fb942a6176b78d455
jefferson <jardim.jefferson@gmail.com>
2024-02-20 20:16:05 -0800

    first

---
+++ js/script.js
@@ -0,0 +1,40 @@
+//step 1
+
+//step 2
+//step 3
+/*
+//1
+let someMonth
+function theMonth()
+let currentMonth
+let summerMonth
+let MyLibraryFunction
+//2
+let num = 1010
+let st = "Jeffeson Santos"
+let bool = false
+let nu = null
+//3
+let some;
+let thing;
+let wd = some + thing;
+//4
+let firstName
+let lastName
+let Address
+let City
+let State
+let zipCode
+let yourAge
+let feferralSource
+let mayWeContactYou
+//5
+firstName = "Jeff"
+lastName = "Santos"
+State = "CA"
+//6
+*/
+let name = "jeff";
+let age = 39;
+let coe = name + age;
+console.log(coe)
