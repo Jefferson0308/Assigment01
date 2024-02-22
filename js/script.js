ab41d3de05a2bb782ba469cdd986042e4ea1d4f9
jefferson <jardim.jefferson@gmail.com>
2024-02-21 18:05:24 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -33,8 +33,40 @@ firstName = "Jeff"
 lastName = "Santos"
 State = "CA"
 //6
+let coe = "jeff" + 39;
+console.log(coe )
+//7
+let boost = false + "fls"
+console.log(boost)
+
+let numboo = 100 + false
+console.log(numboo)
+//8
+no, 
+let someString = 'Who once said, Only two things are infinite, the universe and human stupidity, and I'm" not sure about the former."
+console.log(someString)
+//9
+let v = null
+let vund
+console.log(v)
+console.log(vund)
+//0
+
+//1
+alert()
+//2
+let name = "Jeff";
+alert("Hello " + name +", welcome to the JavaScript class!")
+//3
+let name = "Jeff";
+let cse = "Javascript"
+alert("Hello " + name + ".\nwelcome to the " + cse + " class!");
+//5
+let name = prompt('Type name');
+alert("Hello " + name + ", welcome to the JavaScript class!");
+//6
+let name = prompt("Type name");
+let cse = prompt("Type couRse");
+alert("Hello " + name + ".\nwelcome to the " + cse + " class!");
+//7
 */
-let name = "jeff";
-let age = 39;
-let coe = name + age;
-console.log(coe)
